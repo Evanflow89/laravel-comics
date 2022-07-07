d97214eb9390b209a19188aa2ec14c6792142308
Evanflow89 <ivanbaragone@gmail.com>
2022-07-07 16:49:54 +0200

    Added Partials scss

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,14 @@
 <header>
-    
-</header>
+    <div class="container">
+      <div class="imgLogo">
+        <img src="{{asset('img/dc-logo.png')}}" alt="dc Logo" />
+      </div>
+      <nav class="mainNav">
+        <ul>
+          <li
+      
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
