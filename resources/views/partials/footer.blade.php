567e41c72ac90c8eb1cc0d34728fec0eef686025
Evanflow89 <ivanbaragone@gmail.com>
2022-07-08 12:39:54 +0200

    bug css

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,142 @@
-
-    @include('partials.top-footer');
-    @include('partials.middle-footer');
-    @include('partials.bottom-footer');
+<footer>
+    <section class="topFooter">
+    <div class="container">
+        <div class="card">
+          <div class="img_card">
+            <img
+              src="{{asset('img/buy-comics-digital-comics.png')}}"
+              alt="Dc Digital Comics"
+            />
+          </div>
+          <div class="card_description">Digital Comics</div>
+        </div>
+        <div class="card">
+          <div class="img_card">
+            <img
+              src="{{asset('img/buy-comics-merchandise.png')}}"
+              alt="Dc Merchandise"
+            />
+          </div>
+          <div class="card_description">Dc Merchandise</div>
+        </div>
+        <div class="card">
+          <div class="img_card">
+            <img
+              src="{{asset('img/buy-comics-subscriptions.png')}}"
+              alt="Dc Subscriptions"
+            />
+          </div>
+          <div class="card_description">Subscriptions</div>
+        </div>
+        <div class="card">
+          <div class="img_card">
+            <img
+              src="{{asset('img/buy-comics-shop-locator.png')}}"
+              alt="Dc Shop Locator"
+            />
+          </div>
+          <div class="card_description">Comic Shop Locator</div>
+        </div>
+        <div class="card">
+          <div class="img_card">
+            <img
+              src="{{asset('img/buy-dc-power-visa.svg')}}"
+              alt="Dc Buy Power Visa"
+            />
+          </div>
+          <div class="card_description">Dc Power Visa</div>
+        </div>
+      </div>
+    </section>
+    <section class="middleFooter">
+        <div class="container">
+          <div class="col-left">
+            <div class="footNav">
+              <h2>Dc Comics</h2>
+              <ul>
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">News</a></li>
+              </ul>
+            </div>
+            <div class="footNav">
+              <h2>Shop</h2>
+              <ul>
+                <li><a href="#">Shop DC</a></li>
+                <li><a href="#">Shop DC Collectibles</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-left">
+            <div class="footNav">
+              <h2>Dc</h2>
+              <ul>
+                <li><a href="#">Terms of use</a></li>
+                <li><a href="#">Privacy policy (New)</a></li>
+                <li><a href="#">Ad Choices</a></li>
+                <li><a href="#">Advertising</a></li>
+                <li><a href="#">Jobs</a></li>
+                <li><a href="#">Subscriptions</a></li>
+                <li><a href="#">Talent Workshops</a></li>
+                <li><a href="#">CPSC Certificates</a></li>
+                <li><a href="#">Ratings</a></li>
+                <li><a href="#">Shop Help</a></li>
+                <li><a href="#">Contact Us</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-left">
+            <div class="footNav">
+              <h2>Sites</h2>
+              <ul>
+                <li><a href="#">DC</a></li>
+                <li><a href="#">MAD Magazine</a></li>
+                <li><a href="#">DC Kids</a></li>
+                <li><a href="#">DC Universe</a></li>
+                <li><a href="#">DC Power Visa</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-right">
+            <img src="{{asset('img/dc-logo-bg.png')}}" alt="Dc Logo" />
+          </div>
+        </div>
+      </section>
+      <section class="footerBottom">
+        <div class="container foot">
+          <div class="btn_foot">
+            <button class="signUp">Sign-up now!</button>
+          </div>
+          <div class="social_foot">
+            <div class="follow">
+              <h3>Follow Us</h3>
+            </div>
+            <div class="icon">
+              <img src="{{asset('img/footer-facebook.png')}}" alt="facebook logo" />
+            </div>
+            <div class="icon">
+              <img src="{{asset('img/footer-twitter.png')}}" alt="twitter logo" />
+            </div>
+            <div class="icon">
+              <img src="{{asset('img/footer-youtube.png')}}" alt="youtube logo" />
+            </div>
+            <div class="icon">
+              <img
+                src="{{asset('img/footer-pinterest.png')}}"
+                alt="pinterest logo"
+              />
+            </div>
+            <div class="icon">
+              <img
+                src="{{asset('img/footer-periscope.png')}}"
+                alt="periscope logo"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+</footer>
