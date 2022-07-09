6b4103a319fe642bc2eaeaab1714566aa787a97b
Evanflow89 <ivanbaragone@gmail.com>
2022-07-09 17:55:14 +0200

    Bugs css fixed

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -5,7 +5,23 @@
 @endsection
 
 @section('page-content')
-<div class="container comicPage">
+<section class="comicPage">
+<div class="container">
+    <div class="col-left">
 <h1>{{$comic['title']}}</h1>
+
+<p>{{$comic['description']}}</p>
 </div>
+<div class="col-right">
+    <img src="{{asset('img/superman.jpg')}}" alt="Advertise"> 
+</div>
+</div>
+</section>
 @endsection
+
+
+<style>
+    main {
+        background-color: white !important;
+    }
+</style>
--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -1,5 +1,5 @@
-<main>
+<section class="hero">
     <div class="jumbo_img">
       <img src="{{asset('img/jumbotron.jpg')}}" alt="jumbo image" />
     </div>
-</main>
+  </section>
